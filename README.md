a2a70e70ac91deed364d6c02c52e6974adbddbc9
Sara Ali <sara.ali.mohammed213@gmail.com>
2018-02-24 12:06:55 +0200

    Add NeighborhoodMap files

---
+++ README.md
@@ -0,0 +1,31 @@
+## Neighbourhood Map - favorite places
+This single page application shows a map of favorite places. This app is built for a Neighborhood map project which is a part of the Front End Web Development Nanodegree at Udacity.
+
+## AppFeatures
+
+* Display map markers identifying locations that you are interested in. Your app displays those locations by default when the page is loaded.
+* Implement a list view of the set of locations.
+* Provide a filter option that uses an input field to filter both the list view and the map markers displayed by default on load. The list view and the markers update accordingly.
+* Add functionality using third-party APIs to provide information when a map marker or list view entry is clicked.
+* Add functionality using geometry library to provide 360 degree of street view when a map marker or list view entry is clicked.
+* Add functionality to animate a map marker when either the list item associated with it or the map marker itself is selected.
+* Add functionality to open an infoWindow when either a location is selected from the list view or its map marker is selected directly.
+* Add functionality to change the color of a map marker when mouseover and moueout.
+* The app's interface is intuitive to use. For example, the input text area to filter locations is easy to locate. It should be easy to understand what set of locations is being filtered.
+* Google maps and Foursquare errors are handled gracefully.
+
+## Libraries, frameworks and APIs used
+1. Knockout JS
+2. Google Maps
+3. Geometry
+4. Foursquare
+5. Fontawesome
+6. JQuery
+
+## APIs
+Google Maps API is used here to show the map and generate the markers etc.
+Foursquare API is used to pull more information on my favorite places and provide a more complete listing when you click on the marker and the infoWindow shows up.
+
+## How to View This App
+1. Test the app at:  or
+2. Download the source code from my repo at GitHub: https://github.com/SaraAliMohammed/SaraAli_NeighborhoodMap.git and open /index.html in your browser.
---
+++ js/app.js
@@ -0,0 +1,304 @@
+var myPlaces = [
+    {
+        title: 'Discovery Docks Apartments',
+        location: {lat: 51.501409, lng: -0.018823},
+        type: 'Living Quarters'
+    }, 
+    {
+        title: 'Nandos Restaurant',
+        location: { lat: 51.5023146, lng: -0.0187593 },
+        type: 'Restaurant'
+    },
+    {
+        title: 'One Canada Square',
+        location: { lat: 51.5049494, lng: -0.0194997 },
+        type: 'Shopping'
+    },
+    {
+        title: 'The O2',
+        location: { lat: 51.503039, lng: 0.003154 },
+        type: 'Entertainment'
+    },
+    {
+        title: 'Tesco Express',
+        location: { lat: 51.500575, lng: -0.017354 },
+        type: 'Shopping'
+    },
+    {
+        title: 'Firezza Pizza',
+        location: { lat: 51.496268, lng: -0.015511 },
+        type: 'Restaurant'
+    },
+    {
+        title: 'The Breakfast Club',
+        location: { lat: 51.506066, lng: -0.017345 },
+        type: 'Restaurant'
+    },
+    {
+        title: 'Shake Shack',
+        location: { lat: 51.504905, lng: -0.018967 },
+        type: 'Restaurant'
+    },
+    {
+        title: 'Asda Superstore',
+        location: { lat: 51.494206, lng: -0.012714 },
+        type: 'Shopping'
+    },
+    {
+        title: 'Pizza Express',
+        location: { lat: 51.505233, lng: -0.021716 },
+        type: 'Restaurant'
+    }
+]
+
+// Create a styles array to use with the map.
+var styles = [
+  {
+      featureType: 'water',
+      stylers: [
+        { color: '#19a0d8' }
+      ]
+  }, {
+      featureType: 'administrative',
+      elementType: 'labels.text.stroke',
+      stylers: [
+        { color: '#ffffff' },
+        { weight: 6 }
+      ]
+  }, {
+      featureType: 'administrative',
+      elementType: 'labels.text.fill',
+      stylers: [
+        { color: '#e85113' }
+      ]
+  }, {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [
+        { color: '#efe9e4' },
+        { lightness: -40 }
+      ]
+  }, {
+      featureType: 'transit.station',
+      stylers: [
+        { weight: 9 },
+        { hue: '#e85113' }
+      ]
+  }, {
+      featureType: 'road.highway',
+      elementType: 'labels.icon',
+      stylers: [
+        { visibility: 'off' }
+      ]
+  }, {
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [
+        { lightness: 100 }
+      ]
+  }, {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [
+        { lightness: -100 }
+      ]
+  }, {
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [
+        { visibility: 'on' },
+        { color: '#f0e4d3' }
+      ]
+  }, {
+      featureType: 'road.highway',
+      elementType: 'geometry.fill',
+      stylers: [
+        { color: '#efe9e4' },
+        { lightness: -25 }
+      ]
+  }
+];
+
+//Handle authentication error of google map api
+window.gm_authFailure = function () {
+    $("#map").html("<p>Google maps failed to load!</p><p>Check Api Key</p>");
+    alert('Google maps failed to load!');
+}
+
+var map;
+
+var neighborhoodMapModel = function () {
+    var self = this;
+    this.searchPlace = ko.observable("");
+    this.placesMarkerList = [];
+
+    this.populateInfoWindow = function (marker, infoWindow) {
+        if (infoWindow.marker != marker) {
+            infoWindow.marker = marker;
+            infoWindow.setContent('');
+            
+            // Get Street View
+            var streetViewService = new google.maps.StreetViewService();
+            var radius = 50;
+            // In case the status is OK, which means the pano was found, compute the
+            // position of the streetview image, then calculate the heading, then get a
+            // panorama from that and set the options
+            function getStreetView(data, status) {
+                if (status == google.maps.StreetViewStatus.OK) {
+                    var nearStreetViewLocation = data.location.latLng;
+                    var heading = google.maps.geometry.spherical.computeHeading(
+                      nearStreetViewLocation, marker.position);
+                    infoWindow.setContent('<div id="infoWindowData"></div><div id="pano"></div>');
+                    var panoramaOptions = {
+                        position: nearStreetViewLocation,
+                        pov: {
+                            heading: heading,
+                            pitch: 30
+                        }
+                    };
+                    var panorama = new google.maps.StreetViewPanorama(
+                      document.getElementById('pano'), panoramaOptions);
+                } else {
+                    self.streetViewContent = '<div>' + marker.title + '</div>' +
+                      '<div>No Street View Found</div>';
+                }
+            }
+            // Use streetview service to get the closest streetview image within
+            // 50 meters of the markers position
+            streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+
+            // Foursquare API Client
+            clientID = "HDFWJMDVMRDGQPU5E2P2U0LJGKJAP1HNV4AU1PRWDROUKS52";
+            clientSecret = "PHA4ZHYW2MKKHROGB0EOJGJOD3B2JSTNJOU4TVQWT3FCHAB5";
+            var foursqureApiUrl = 'https://api.foursquare.com/v2/venues/search?ll=' +
+                marker.position.lat() + ',' + marker.position.lng() + '&client_id=' + clientID +
+                '&client_secret=' + clientSecret + '&query=' + marker.title +
+                '&v=20170708' + '&m=foursquare';
+
+            // Call Foursquare API
+            $.getJSON(foursqureApiUrl).done(function (marker) {
+                var response = marker.response.venues[0];
+                self.street = response.location.formattedAddress[0];
+                self.city = response.location.formattedAddress[1];
+                self.zip = response.location.formattedAddress[3];
+                self.country = response.location.formattedAddress[4];
+                self.category = response.categories[0].shortName;
+
+                self.htmlContentFoursquare =
+                    '<h5>(' + self.category +
+                    ')</h5>' + '<div>' +
+                    '<h6> Address: </h6>' +
+                    '<p>street : ' + self.street + '</p>' +
+                    '<p>city : ' + self.city + '</p>' +
+                    '<p>zip : ' + self.zip + '</p>' +
+                    '<p>country : ' + self.country +
+                    '</p>' + '</div>' + '</div>';
+                
+                $("#infoWindowData").append(self.htmlContent + self.htmlContentFoursquare);
+            }).fail(function () {
+                // alert error message 
+                alert(
+                    "There was an issue loading the Foursquare API. Please refresh your page to try again."
+                );
+            });
+
+            this.htmlContent = '<div>' + '<h4>' + marker.title +
+                '</h4>';
+
+            // Open the infowindow on the correct marker.
+            infoWindow.open(map, marker);
+            infoWindow.addListener('closeclick', function () {
+                infoWindow.marker = null;
+            });
+        }
+    };
+
+    this.populateAndBounceMarker = function () {
+        self.populateInfoWindow(this, self.placeInfoWindow);
+        this.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout((function () {
+            this.setAnimation(null);
+        }).bind(this), 1400);
+    };
+
+    this.makeMarkerIcon = function(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          '|40|_|%E2%80%A2',
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21,34));
+        return markerImage;
+    };
+
+    this.initMap = function () {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: new google.maps.LatLng(51.4980479, -0.0105351),
+            zoom: 15,
+            styles: styles
+        };
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(mapCanvas, mapOptions);
+
+        this.placeInfoWindow = new google.maps.InfoWindow();
+        this.bounds = new google.maps.LatLngBounds();
+
+        var length = myPlaces.length;
+
+        // Style the markers a bit. This will be my listing marker icon.
+        var defaultIcon = self.makeMarkerIcon('f75850');
+        // Create a "highlighted location" marker color for when the user
+        // mouses over the marker.
+        var highlightedIcon = self.makeMarkerIcon('ffff24');
+
+        //Creating list places with markers
+        for (var i = 0 ; i < length ; i++) {
+            this.position = myPlaces[i].location;
+            this.title = myPlaces[i].title;
+
+            //Create marker for the place
+            this.marker = new google.maps.Marker({
+                map: map,
+                position: this.position,
+                title: this.title,
+                id: i,
+                icon: defaultIcon,
+                animation: google.maps.Animation.DROP
+            });
+
+            //Add click listener of marker to open info window
+            this.marker.setMap(map);
+            this.placesMarkerList.push(this.marker);
+            this.marker.addListener('click', self.populateAndBounceMarker);
+            this.bounds.extend(this.marker.position);
+            this.marker.addListener('mouseover', function () {
+                this.setIcon(highlightedIcon);
+            });
+            this.marker.addListener('mouseout', function () {
+                this.setIcon(defaultIcon);
+            });
+        }
+
+        map.fitBounds(this.bounds);
+    };
+
+    this.initMap();
+
+    this.myPlacesFilter = ko.computed(function () {
+        var places = [];
+        self.placesMarkerList.forEach(function (Marker) {
+            if (Marker.title.toLowerCase().includes(self.searchPlace().toLowerCase())) {
+                places.push(Marker);
+                Marker.setVisible(true);
+            } else
+                Marker.setVisible(false);
+        });     
+        return places;
+    });
+}
+
+function showNeighborhoodMap() {
+    ko.applyBindings(new neighborhoodMapModel());
+}
